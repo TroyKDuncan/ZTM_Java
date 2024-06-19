be0cfd5ca2aea9edb3f670128a55a98a2322a359
Troy Duncan <troyduncanbusiness@gmail.com>
2024-06-18 23:31:47 -0500

    Finished access modifiers, string, stringbuilder, and stringbuffer

--- Notes/src/main/java/org/example/AccessModifiers.java
+++ Notes/src/main/java/org/example/AccessModifiers.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class AccessModifiers {
     public static void main(String[] args) {
+        // best practice is to keep things as private as possible to keep things at a high level
 
+        // public: accessible from anywhere just needs to be imported
+
+        // private: accessible within the same class, just like C++
+        // use getters and setters to access private values
+        // private methods can only be called WITHIN the same class
+
+        // protected: accessible inside the package and to its subclasses outside the package
+        // default: no modifier, accessible inside the same package, can't be imported outside of the package
+
+        // static: member belongs to the class, not the instance
+        // all objects of the given class has the same value for a static value
+        // statics can be modified without creating an object of the given type
+        // static methods can be used without creating an object as well
     }
 }
--- Notes/src/main/java/org/example/Strings.java
+++ Notes/src/main/java/org/example/Strings.java
@@ -6,6 +6,22 @@ public class Strings {
         String s2 = "goodbye";
         System.out.println(s1.equals(s2));
 
+        // COMMON METHODS
+        // length()
+        // charAt(int index)
+        // indexOf(String)
+        // substring(beginning, ending(optional, exclusive))
+        // toLowerCase(), toUpperCase()
+        // trim() removes leading and trailing whitespace
 
+        // strings are thread-safe because they are immutable
+        // assigning a variable a string will always create a new object
+
+        // obj1.equals(obj2) returns boolean
+
+        // StringBuilder
+        // data type for mutable text
+        // you can use append, insert, delete, replace
+        // StringBuffer is thread-safe
     }
 }
