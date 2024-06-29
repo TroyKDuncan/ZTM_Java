a084b5f05c44a6aba5810f340478b412e5e05e12
Troy Duncan <troyduncanbusiness@gmail.com>
2024-06-29 13:39:48 -0500

    Working on exceptions

---
+++ Notes/src/main/java/Exceptions/CheckedAndUnchecked.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+public class CheckedAndUnchecked {
+    public static void main(String[] args) {
+        // checked need to be handled and happen at compile time
+        // unchecked happen at runtime
+
+        // throwing exceptions should only be used if you can't use try/catch because it still crashes your
+        // program.
+
+        // try/catch works the same as in python try/except
+        // you have to use individual catch statements for each exception, instead of listing them like in python
+    }
+}
